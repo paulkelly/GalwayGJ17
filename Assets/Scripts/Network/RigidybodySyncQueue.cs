bf6f1ddf89d8d5559b8eda5f79c390df2238485d
Paul Kelly <pkelly429@gmail.com>
2021-06-16 21:34:01 +0100

    Creating too much network traffic, trying something different.

--- Assets/Scripts/Network/RigidybodySyncQueue.cs
+++ Assets/Scripts/Network/RigidybodySyncQueue.cs
@@ -5,50 +5,50 @@ using UnityEngine;
 
 public class RigidybodySyncQueue : MonoBehaviour
 {
-    private const int NumberToSyncPerUpdate = 2;
-
-    private static RigidybodySyncQueue _instance;
-    private static List<Debris> _syncQueue = new List<Debris>();
-
-    [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.AfterSceneLoad)]
-    private static void OnGameStart()
-    {
-        GameObject obj = new GameObject("Rigidbody Sync");
-        obj.AddComponent<RigidybodySyncQueue>();
-    }
-
-    private void Awake()
-    {
-        if (_instance == null)
-        {
-            _instance = this;
-            DontDestroyOnLoad(gameObject);
-        }
-        else
-        {
-            Destroy(gameObject);
-        }
-    }
-
-    public static void Subscribe(Debris rigidbody)
-    {
-        _syncQueue.Add(rigidbody);
-    }
-
-    public static void UnSubscribe(Debris rigidbody)
-    {
-        _syncQueue.Remove(rigidbody);
-    }
-
-    private void Update()
-    {
-        int toProcess = Math.Max(NumberToSyncPerUpdate, _syncQueue.Count);
-        for (int i = 0; i < toProcess; i++)
-        {
-            Debris toSync = _syncQueue[0];
-            toSync.Sync();
-            _syncQueue.RemoveAt(0);
-            _syncQueue.Add(toSync);
-        }
-    }
+    // private const int NumberToSyncPerUpdate = 2;
+    //
+    // private static RigidybodySyncQueue _instance;
+    // private static List<Debris> _syncQueue = new List<Debris>();
+    //
+    // [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.AfterSceneLoad)]
+    // private static void OnGameStart()
+    // {
+    //     GameObject obj = new GameObject("Rigidbody Sync");
+    //     obj.AddComponent<RigidybodySyncQueue>();
+    // }
+    //
+    // private void Awake()
+    // {
+    //     if (_instance == null)
+    //     {
+    //         _instance = this;
+    //         DontDestroyOnLoad(gameObject);
+    //     }
+    //     else
+    //     {
+    //         Destroy(gameObject);
+    //     }
+    // }
+    //
+    // public static void Subscribe(Debris rigidbody)
+    // {
+    //     _syncQueue.Add(rigidbody);
+    // }
+    //
+    // public static void UnSubscribe(Debris rigidbody)
+    // {
+    //     _syncQueue.Remove(rigidbody);
+    // }
+    //
+    // private void Update()
+    // {
+    //     int toProcess = Math.Max(NumberToSyncPerUpdate, _syncQueue.Count);
+    //     for (int i = 0; i < toProcess; i++)
+    //     {
+    //         Debris toSync = _syncQueue[0];
+    //         toSync.Sync();
+    //         _syncQueue.RemoveAt(0);
+    //         _syncQueue.Add(toSync);
+    //     }
+    // }
 }
